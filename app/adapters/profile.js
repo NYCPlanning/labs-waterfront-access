c08a73931d0f283b84acbf439260031bca547c44
allthesignals <wilburnforce@gmail.com>
2018-10-09 15:51:01 -0400

    Configure profile adapter/serializer

--- app/adapters/profile.js
+++ app/adapters/profile.js
@@ -1,6 +1,24 @@
 import DS from 'ember-data';
-const { Adapter } = DS;
+import { buildSqlUrl } from 'cartobox-promises-utility/utils/carto';
 
-export default class ProfileAdapter extends Adapter {
+const SQL = function(id) {
+  return `SELECT 
+    st_x(st_centroid(the_geom)) as lon, 
+    st_y(st_centroid(the_geom)) as lat,
+    bbl AS id 
+    FROM mappluto_v18_1 WHERE bbl=${id}`;
+};
 
+const { JSONAPIAdapter } = DS;
+
+export default class ProfileAdapter extends JSONAPIAdapter {
+  keyForAttribute(key) {
+    return key;
+  }
+
+  urlForFindRecord(id) {
+    return buildSqlUrl(
+      SQL(id),
+    );
+  }
 }
