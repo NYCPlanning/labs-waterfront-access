46c4902d4b1318c9dbe0fa883918c0ce2f29b6de
Hannah Kates <hannah.b.kates@gmail.com>
2018-10-09 15:18:19 -0400

    create serializer and adpater for profile

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,14 +1,4 @@
-// import DS from 'ember-data';
-// const { Serializer } = DS;
-
-// export default class ApplicationSerializer extends Serializer {
-// 	App.ApplicationSerializer = DS.RESTSerializer.extend({
-// 	  primaryKey: 'bbl'
-// 	});
-// }
-
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
-  // primaryKey: 'bbl'
 });
---
+++ app/serializers/profile.js
@@ -0,0 +1,6 @@
+import DS from 'ember-data';
+const { Serializer } = DS;
+
+export default class ProfileSerializer extends Serializer {
+
+}
