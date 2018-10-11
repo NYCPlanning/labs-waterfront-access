67d45becbd473bb28ba59b84761bd7f7b7c21d99
Chris Whong <chris@whong.org>
2018-10-11 17:13:18 -0400

    get DO spaces api call working

--- app/routes/profiles/show.js
+++ app/routes/profiles/show.js
@@ -1,14 +1,25 @@
 import Route from '@ember/routing/route';
+import convert from 'xml-js';
+import { isArray } from '@ember/array';
 
 export default class ShowProjectRoute extends Route {
   async model({ id }) {
     const model = await this.store.findRecord('profile', id, { reload: true });
-    const images = await fetch(`https://api.github.com/repos/nycplanning/labs-waterfront-access-photos/contents/${id}`)
-      .then(d => d.json())
+    const images = await fetch(`https://waterfront-access-photos.nyc3.digitaloceanspaces.com/?prefix=${id}`)
+      .then(d => d.text())
+      .then(d => {
+        const json = convert.xml2js(d, { compact: true })
+        let { Contents: contents } = json.ListBucketResult;
 
-    if (!images.message) {
-      model.set('images', images.map(d => d.download_url))
-    }
+        const contentsArray = isArray(contents) ? contents : [contents];
+
+        return contentsArray.map(d => {
+          const filename = d.Key._text;
+          return `https://waterfront-access-photos.nyc3.digitaloceanspaces.com/${filename}`;
+        })
+      });
+
+    model.set('images', images);
 
     return model;
   }
