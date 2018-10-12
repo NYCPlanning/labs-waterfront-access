8d6bd73fc5d4fdd58c16f5c0053ea06b66b3616b
allthesignals <wilburnforce@gmail.com>
2018-10-12 13:08:06 -0400

    Add a restartable task for checking for map availability

--- app/routes/profiles/show.js
+++ app/routes/profiles/show.js
@@ -1,7 +1,22 @@
 import Route from '@ember/routing/route';
+import { timeout } from 'ember-concurrency';
+import { restartableTask } from 'ember-concurrency-decorators';
 
 export default class ShowProjectRoute extends Route {
   model({ id }) {
     return this.store.findRecord('profile', id, { reload: true });
+  }
+
+  @restartableTask
+  fitBoundsWhenReady = function* (geometry) {
+    const applicationController = this.controllerFor('application');
+    while (!applicationController.get('mapInstance')) {
+      yield timeout(100);
+    }
+    // console.log(applicationController.get('mapInstance'));
+  }
+
+  afterModel(model, transition) {
+    this.get('fitBoundsWhenReady').perform(model.get('geometry'));
   }
 }
