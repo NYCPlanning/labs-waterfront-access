94654bd80870b9cab0d1e595532f1f1f6ba3d7a5
Hannah Kates <hannah.b.kates@gmail.com>
2018-10-17 14:40:45 -0400

    create popup component and model for govt owned parks aka external records

--- tests/unit/routes/profiles/external-test.js
+++ tests/unit/routes/profiles/external-test.js
@@ -5,7 +5,7 @@ module('Unit | Route | profiles/external', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:profiles/external');
+    const route = this.owner.lookup('route:profiles/external');
     assert.ok(route);
   });
 });
