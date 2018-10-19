a660cffcedb4757179892de371068aa89d2f0878
allthesignals <wilburnforce@gmail.com>
2018-10-19 16:23:43 -0400

    Remove mustache dependency; replace with htmlbars

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,6 +10,8 @@ module.exports = function(defaults) {
     },
   });
 
+  app.import('vendor/ember/ember-template-compiler.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
