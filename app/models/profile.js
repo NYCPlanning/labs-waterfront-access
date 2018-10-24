9c196e1c99bd944a0ca33c00627dbc001b0033aa
Hannah Kates <hannah.b.kates@gmail.com>
2018-10-24 16:07:02 -0400

    wip

--- app/models/profile.js
+++ app/models/profile.js
@@ -17,7 +17,7 @@ export default class ProfileModel extends Model {
 
   @attr('string') address;
 
-  @attr('string') agency;
+  @attr('string') maintained;
 
   @attr('string') category;
 
@@ -84,6 +84,37 @@ export default class ProfileModel extends Model {
   @attr('boolean') trash_receptacles;
 
   @attr('boolean') trees;
+
+// constructi,
+// adjacent_w,
+// the_geom,
+// cpc_report,
+// cpc_approv,
+// chair_cert,
+// hours_open,
+// restrictiv,
+// summary_of,
+// shore_pu_1,
+// shore_publ,
+// feature_promenade_esplanade,
+// feature_seating_lawn,
+// feature_pier,
+// feature_wetland_natural_edge,
+// feature_dog_run,
+// feature_educational_or_interpretive,
+// feature_public_restroom,
+// feature_shade_structure,
+// feature_outdoor_art,
+// feature_food_or_beverage_concessions,
+// feature_group_seating,
+// activity_volleyball_court,
+// activity_basketball_court,
+// activity_fishing,
+// activity_boating_access,
+// activity_tot_playground,
+// activity_splash_feature,
+// activity_other_recreational_facilities,
+// activity_swimming
 
   @attr('boolean') feature_promenade_esplanade;
 
