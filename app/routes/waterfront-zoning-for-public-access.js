12abf9a20329bb5a3ef048bfec3f4498d091cea4
Andy Cochran <acochran@planning.nyc.gov>
2018-10-25 13:53:14 -0400

    show/hide sidebar on small screens

--- app/routes/waterfront-zoning-for-public-access.js
+++ app/routes/waterfront-zoning-for-public-access.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember-decorators/object'; // eslint-disable-line
 
-export default Route.extend({
-});
+export default class WaterfrontZoningInfoRoute extends Route {
+  @action
+  didTransition() {
+    const applicationController = this.controllerFor('application');
+    applicationController.set('sidebarIsClosed', true);
+  }
+}
