fb02e3d49f70f65b9ebb481142b963cd7214bcf3
Hannah Kates <hannah.b.kates@gmail.com>
2018-10-24 21:15:40 -0400

    use id instead of paws_id

--- app/models/profile.js
+++ app/models/profile.js
@@ -11,8 +11,6 @@ const S3_BUCKET_HOST = 'https://waterfront-access-photos.nyc3.digitaloceanspaces
 export default class ProfileModel extends Model {
   @attr() geometry;
 
-  @attr('string') paws_id;
-
   @attr('string') park_name;
 
   @attr('string') address;
@@ -85,7 +83,7 @@ export default class ProfileModel extends Model {
 
   @restartableTask()
   images = function* () { // eslint-disable-line
-    const id = this.get('paws_id');
+    const id = this.get('id');
 
     return fetch(`${S3_BUCKET_HOST}/?prefix=${id}`)
       .then(d => d.text())
