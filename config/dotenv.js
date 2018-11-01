044f1d7463ef52cec8796fe368c04e6924657ab7
trbmcginnis <taylormcgin@gmail.com>
2018-11-01 15:41:11 -0400

    add dotenv

---
+++ config/dotenv.js
@@ -0,0 +1,9 @@
+'use strict';
+module.exports = function() {
+  return {
+    clientAllowedKeys: ['BROWSERSTACK_USERNAME', 'BROWSERSTACK_ACCESS_KEY'],
+    // Fail build when there is missing any of clientAllowedKeys environment variables.
+    // By default false.
+    failOnMissingKey: false,
+  };
+};
