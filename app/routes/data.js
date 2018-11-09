9c36cd6cbd37d21eb078793d77162b27abf4dd29
Andy Cochran <acochran@planning.nyc.gov>
2018-11-08 19:06:31 -0500

    move the resize event from every route to the extended router

--- app/routes/data.js
+++ app/routes/data.js
@@ -1,6 +1,6 @@
 import Route from '@ember/routing/route';
 import { action } from '@ember-decorators/object'; // eslint-disable-line
-import { next } from '@ember/runloop';
+// import { next } from '@ember/runloop';
 
 export default class DataRoute extends Route {
   async model() {
@@ -12,14 +12,5 @@ export default class DataRoute extends Route {
   didTransition() {
     const applicationController = this.controllerFor('application');
     applicationController.set('sidebarIsClosed', true);
-
-    next(function() {
-      // not supported in IE 11
-      // window.dispatchEvent(new Event('resize'));
-
-      const resizeEvent = window.document.createEvent('UIEvents');
-      resizeEvent.initUIEvent('resize', true, false, window, 0);
-      window.dispatchEvent(resizeEvent);
-    });
   }
 }
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,22 +1,11 @@
 import Route from '@ember/routing/route';
 import { action } from '@ember-decorators/object'; // eslint-disable-line
-import { next } from '@ember/runloop';
+// import { next } from '@ember/runloop';
 
 export default class IndexRoute extends Route {
   @action
   didTransition() {
     const applicationController = this.controllerFor('application');
     applicationController.set('sidebarIsClosed', true);
-
-    next(() => {
-      // not supported in IE 11
-      // window.dispatchEvent(new Event('resize'));
-
-      const resizeEvent = window.document.createEvent('UIEvents');
-      resizeEvent.initUIEvent('resize', true, false, window, 0);
-      window.dispatchEvent(resizeEvent);
-
-      applicationController.set('highlightedFeature', null);
-    });
   }
 }
