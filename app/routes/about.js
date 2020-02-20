f55e48f15e13a95e118eb8e8a73b8764ff501d11
Brandyn Friedly <bfriedly@planning.nyc.gov>
2020-02-20 16:33:44 -0500

    Fix route name
    The route name was incorrectly named "ShowProfileRoute."
    
    This commit fixes the name to "AboutRoute"

--- app/routes/about.js
+++ app/routes/about.js
@@ -25,7 +25,7 @@ const query = `
   FROM publiclyowned, paws
 `;
 
-export default class ShowProjectRoute extends Route {
+export default class AboutRoute extends Route {
   async model() {
     const [data] = await carto.SQL(query);
     return data;
