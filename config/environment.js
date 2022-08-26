4cbd25ef5a392e111be01a6d551790b594cf959e
godfreyyeung <godfreyyeung@gmail.com>
2022-08-26 17:34:53 -0400

    9404 Remove production-specific labs-search config

--- config/environment.js
+++ config/environment.js
@@ -86,11 +86,6 @@ module.exports = function(environment) {
   if (environment === 'production') {
     ENV.host = 'https://layers-api.planninglabs.nyc';
     ENV['mapbox-gl'].map.style = 'https://layers-api.planninglabs.nyc/v1/base/style.json';
-    ENV['labs-search'] = {
-      host: 'https://search-api.planninglabs.nyc',
-      route: 'search',
-      helpers: ['geosearch', 'waterfront-park-name'],
-    };
     ENV['ember-mapbox-composer'] = {
       host: 'https://layers-api.planninglabs.nyc',
     };
