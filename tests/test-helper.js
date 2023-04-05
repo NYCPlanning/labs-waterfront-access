e297c0204760de6a56d279d8cac6d32d86ddd02e
Tim Miller <miller.tim108@gmail.com>
2023-04-05 10:29:38 -0400

    v3.19.0...v3.20.0

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,7 +1,7 @@
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
-import config from '../config/environment';
-import Application from '../app';
+import Application from 'labs-waterfront-access/app';
+import config from 'labs-waterfront-access/config/environment';
 
 setApplication(Application.create(config.APP));
 
